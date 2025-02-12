8fe3d1d47b1bcf0586c462bc52f8ddfbea868020
patriciogabriel <patriciogabriel@usto.re>
2025-02-12 15:58:10 -0300

    start project

---
+++ src/main/java/com/ptrc/dev/wedding/invitation/Application.java
@@ -0,0 +1,13 @@
+package com.ptrc.dev.wedding.invitation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptrc/dev/wedding/invitation/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptrc.dev.wedding.invitation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
